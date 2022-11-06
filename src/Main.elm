33aae16493acd710b5a1fef9eef867fdab0fa9dc
Bruno Claudino <bruarrais@gmail.com>
2022-11-05 22:49:27 -0300

    add external requests

--- src/Main.elm
+++ src/Main.elm
@@ -2,19 +2,24 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes as Attr exposing (..)
+import Html.Attributes exposing (class, placeholder, type_, value)
 import Html.Events exposing (onInput)
+import Http
+import Json.Decode as Decode exposing (Decoder, decodeString, float, int, list, nullable, string)
+import Json.Decode.Pipeline as Pdecode exposing (hardcoded, optional, required)
 
 
 
 -- MAIN
 
 
+main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = init
+    Browser.element
+        { init = \flags -> ( initialModel, Cmd.none )
         , update = update
         , view = view
+        , subscriptions = \model -> Sub.none
         }
 
 
@@ -30,15 +35,71 @@ type alias Model =
 
 
 type alias Pep =
-    { nome : String
-    , sobrenome : String
-    , cpf : String
+    { cpf_parcial : String
+    , data_carencia : String
+    , data_fim : String
+    , data_inicio : String
+    , nome : String
+    , regiao : String
+    , sigla : String
     }
+
+
+pepDecoder : Decoder Pep
+pepDecoder =
+    Decode.succeed Pep
+        |> Pdecode.required "cpf_parcial" string
+        |> Pdecode.hardcoded ""
+        |> Pdecode.hardcoded ""
+        |> Pdecode.hardcoded ""
+        |> Pdecode.required "nome" string
+        |> Pdecode.hardcoded ""
+        |> Pdecode.hardcoded ""
 
 
 init : Model
 init =
-    Model [ Pep "Astolfo" "Sobrenome" "123456", Pep "Arnaldo" "Maninho" "123432" ] "" ""
+    Model [ createPep "" "" "" "" "" "" (Just "") ] "" ""
+
+
+initialModel : Model
+initialModel =
+    Model [ createPep "" "" "" "" "" "" (Just "") ] "" ""
+
+
+createPep : String -> String -> String -> String -> String -> String -> Maybe String -> Pep
+createPep cpf_parcial data_carencia data_fim data_inicio nome regiao sigla =
+    case sigla of
+        Just sig ->
+            Pep cpf_parcial data_carencia data_fim data_inicio nome regiao sig
+
+        Nothing ->
+            Pep cpf_parcial data_carencia data_fim data_inicio nome regiao ""
+
+
+searchPepByNome : String -> Cmd Msg
+searchPepByNome nome =
+    if String.length nome > 3 then
+        Http.get
+            { url = "http://localhost:4000/api/pep/nome/" ++ nome
+            , expect = Http.expectJson SearchedByName (Decode.list pepDecoder)
+            }
+
+    else
+        Cmd.none
+
+
+searchPepByCpf : String -> Cmd Msg
+searchPepByCpf cpf_parcial =
+    case String.length cpf_parcial of
+        6 ->
+            Http.get
+                { url = "http://localhost:4000/api/pep/" ++ cpf_parcial
+                , expect = Http.expectJson SearchedByName (Decode.list pepDecoder)
+                }
+
+        _ ->
+            Cmd.none
 
 
 
@@ -48,23 +109,37 @@ init =
 type Msg
     = InputSearchByCpf String
     | InputSearchByName String
+    | SearchedByName (Result Http.Error (List Pep))
+    | SearchedByCpf (Result Http.Error (List Pep))
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         InputSearchByName name ->
-            { model | buscaNome = name}
-            --{ model | buscaNome = name, peps = [ Pep name "Clodovislis" "123123123" ] }
+            ( { model | buscaNome = name }, searchPepByNome name )
 
+        --{ model | buscaNome = name, peps = [ Pep name "Clodovislis" "123123123" ] }
         InputSearchByCpf cpf ->
-            { model | buscaCpf = cpf}
-            -- Nao preciso criar duas mensagens, basta atualizar o model com as informacoes.
-            -- A ideia é que seja em tempo real, entao vai ser onInput, mas poderia ser com outros eventos :)
-            --{ model | buscaCpf = cpf, peps = [ Pep "Mirovaldo" "Rabindramba" cpf ] }
+            ( { model | buscaCpf = cpf }, searchPepByCpf cpf )
+
+        SearchedByName (Ok peps) ->
+            ( { model | peps = peps }, Cmd.none )
+
+        SearchedByName (Err _) ->
+            ( model, Cmd.none )
+
+        SearchedByCpf (Ok peps) ->
+            ( { model | peps = peps }, Cmd.none )
+
+        SearchedByCpf (Err _) ->
+            ( model, Cmd.none )
 
 
 
+-- Nao preciso criar duas mensagens, basta atualizar o model com as informacoes.
+-- A ideia é que seja em tempo real, entao vai ser onInput, mas poderia ser com outros eventos :)
+--{ model | buscaCpf = cpf, peps = [ Pep "Mirovaldo" "Rabindramba" cpf ] }
 -- VIEW
 
 
@@ -103,4 +178,4 @@ viewHeaderPeps =
 
 viewRowsPeps : Pep -> Html msg
 viewRowsPeps pep =
-    tr [ class "table-row" ] [ td [] [ text pep.nome ], td [] [ text pep.cpf ] ]
+    tr [ class "table-row" ] [ td [] [ text pep.nome ], td [] [ text pep.cpf_parcial ] ]
